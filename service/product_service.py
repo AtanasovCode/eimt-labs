784e137b0f33a1660e9d2c2ff23619757d5ac284
AtanasovCode <a_ljupce@yahoo.com>
2026-05-17 00:57:12 +0200

    create product service file & functions - save function - update function

---
+++ service/product_service.py
@@ -0,0 +1,36 @@
+from model.schemas import Product, ProductDto
+from repository import product_repo as products
+from repository import category_repo as categories, manufacturer_repo as manufacturers
+
+def list_all():
+    return products.list_all()
+
+def find_by_id(product_id: int):
+    return products.find_by_id(product_id)
+
+def delete_product(product_id: int):
+    return products.delete_product(product_id)
+
+def save(product_dto: ProductDto):
+    category = categories.find_by_id(product_dto.category_id)
+    manufacturer = manufacturers.find_by_id(product_dto.manufacturer_id)
+    
+    product = Product(
+        id = product_dto.id,
+        name = product_dto.name,
+        price = product_dto.price,
+        category = category,
+        manufacturer = manufacturer
+    )
+    
+    return products.save(product)
+
+def update(product_dto: ProductDto):
+    product = products.find_by_id(product_dto.id)
+    product.id = product_dto.id
+    product.name = product_dto.name
+    product.price = product_dto.price
+    product.category = categories.find_by_id(product_dto.category_id)
+    product.manufacturer = manufacturers.find_by_id(product_dto.manufacturer_id)
+    
+    return product
