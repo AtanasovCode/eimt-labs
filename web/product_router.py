e90ddbe087f46c1cab0a1b0872d3edde8fe72c11
AtanasovCode <a_ljupce@yahoo.com>
2026-05-17 01:21:20 +0200

    add GET api/products/{product_id} route

--- web/product_router.py
+++ web/product_router.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from fastapi.responses import JSONResponse
 from model.schemas import Product
 from service import product_service as products
 
@@ -8,3 +9,12 @@ router = APIRouter(prefix="/api/products", tags=["Products"])
 @router.get("/", response_model=list[Product])
 async def list_all():
     return products.list_all()
+
+@router.get("/{product_id}", response_model=Product)
+async def find_by_id(product_id: int):
+    product = products.find_by_id(product_id)
+    
+    if product is not None:
+        return product
+    
+    return JSONResponse(status_code=404, content={"message": f"Product with id {product_id} not found"})
