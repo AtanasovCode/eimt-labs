b422af6e6daadd5e1b9e99bc08d06f5a16744b9c
AtanasovCode <a_ljupce@yahoo.com>
2026-05-18 16:03:02 +0200

    make pydantic schemas compatible with sqlalchemy

--- model/schemas.py
+++ model/schemas.py
@@ -1,15 +1,40 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class CategorySchema(BaseModel):
     id: int
     name: str
     description: str
     
+    class Config:
+        from_attributes = True
+    
+    
+class ProductCreate(BaseModel):
+    name: str
+    price: float
+    quantity: int
+    category_id: int
+    manufacturer_id: int
+    
+    
+class ProductUpdate(BaseModel):
+    name: Optional[str] = None
+    price: Optional[float] = None
+    quantity: Optional[int] = None
+    category_id: Optional[int] = None
+    manufacturer_id: Optional[int] = None
+    
+    
 
 class ManufacturerSchema(BaseModel):
     id: int
     name: str
     address: str
+    
+    class Config:
+        from_attributes = True
+        
 
 class ProductSchema(BaseModel):
     id: int
@@ -17,6 +42,9 @@ class ProductSchema(BaseModel):
     price: float
     category: CategorySchema
     manufacturer: ManufacturerSchema
+    
+    class Config:
+        from_attributes = True
 
 
 class ProductDtoSchema(BaseModel):
@@ -25,3 +53,6 @@ class ProductDtoSchema(BaseModel):
     price: float
     category_id: int
     manufacturer_id: int
+    
+    class Config:
+        from_attributes = True
