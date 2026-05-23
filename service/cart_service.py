1c02517318bf8b5b9ab5d21e0a7c6393c998de4d
AtanasovCode <a_ljupce@yahoo.com>
2026-05-23 22:26:04 +0200

    create service layer for carts

---
+++ service/cart_service.py
@@ -0,0 +1,31 @@
+from sqlalchemy.orm import Session
+from repository import cart_repo as carts
+from model.schemas import CartItemCreate
+
+
+def get_cart_by_user(db: Session, user_id: int):
+    return carts.get_cart_by_user(db, user_id)
+
+
+def create_cart(db: Session, user_id: int):
+    return carts.create_cart(db, user_id)
+
+
+def add_item_to_cart(db: Session, cart_id: int, item_data: CartItemCreate):
+    return carts.add_item_to_cart(db, cart_id, item_data)
+
+
+def remove_item_from_cart(db: Session, cart_id: int, item_id: int):
+    return carts.remove_item_from_cart(db, cart_id, item_id)
+
+
+def clear_cart(db: Session, cart_id: int):
+    return carts.clear_cart(db, cart_id)
+
+
+def buy_items(db: Session, cart_id: int):
+    return carts.buy_items(db, cart_id)
+
+
+def get_cart_items(db: Session, cart_id: int):
+    return carts.get_cart_items(db, cart_id)
